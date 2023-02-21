ba7dfc5445b8ed0bce4df01c3a8802326de04fdf
wendellopes <wendelopes@msn.com>
2023-02-21 17:11:43 -0300

    database create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
