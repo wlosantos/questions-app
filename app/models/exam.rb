6d367d16717386b8a3dea1aea75621478b51f5f4
wendellopes <wendelopes@msn.com>
2023-02-27 16:24:55 -0300

    adjust exam request rspec

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -3,6 +3,7 @@ class Exam < ApplicationRecord
   has_many :questions, dependent: :destroy
 
   validates :theme, presence: true
+  enum status: { pending: 0, waiting: 1, approved: 2, rejected: 3 }
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[theme status]
