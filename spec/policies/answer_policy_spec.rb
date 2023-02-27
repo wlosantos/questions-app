f483225f1d8484dad240046fa65066fac2ff4245
wendellopes <wendelopes@msn.com>
2023-02-27 16:56:29 -0300

    adjust policies and tests

--- spec/policies/answer_policy_spec.rb
+++ spec/policies/answer_policy_spec.rb
@@ -13,28 +13,12 @@ RSpec.describe AnswerPolicy, type: :policy do
       user.add_role :admin
       expect(subject).to permit(user)
     end
-
-    it 'grants access if user is the same as the record' do
-      expect(subject).to permit(user, answer.question)
-    end
-
-    it 'denies access if user is not an admin and not the same as the record' do
-      expect(subject).not_to permit(user2, answer.question)
-    end
   end
 
   permissions :show? do
     it 'grants access if user is an admin' do
       user.add_role :admin
       expect(subject).to permit(user)
-    end
-
-    it 'grants access if user is the same as the record' do
-      expect(subject).to permit(user, answer.question)
-    end
-
-    it 'denies access if user is not an admin and not the same as the record' do
-      expect(subject).not_to permit(user2, answer.question)
     end
   end
 
@@ -54,28 +38,12 @@ RSpec.describe AnswerPolicy, type: :policy do
       user.add_role :admin
       expect(subject).to permit(user)
     end
-
-    it 'grants access if user is the same as the record' do
-      expect(subject).to permit(user, answer.question)
-    end
-
-    it 'denies access if user is not an admin and not the same as the record' do
-      expect(subject).not_to permit(user2, answer.question)
-    end
   end
 
   permissions :destroy? do
     it 'grants access if user is an admin' do
       user.add_role :admin
       expect(subject).to permit(user)
-    end
-
-    it 'grants access if user is the same as the record' do
-      expect(subject).to permit(user, answer.question)
-    end
-
-    it 'denies access if user is not an admin and not the same as the record' do
-      expect(subject).not_to permit(user2, answer.question)
     end
   end
 end
