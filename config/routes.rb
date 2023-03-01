7b2a2fcbe4f5b76b845094838d051801fb1d1b81
wendellopes <wendelopes@msn.com>
2023-03-01 03:02:12 -0300

    documentation registration rest api with swagger

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :api, defaults: { format: :json }, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resource :registrations, only: [:create]
