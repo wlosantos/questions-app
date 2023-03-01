3035515ead6c85bd88764f76a5e72b576961c81e
wendellopes <wendelopes@msn.com>
2023-03-01 03:36:20 -0300

    documentation session rest api with swagger

---
+++ spec/integration/sessions_spec.rb
@@ -0,0 +1,23 @@
+require 'swagger_helper'
+
+RSpec.describe 'Api::V1::Sessions', type: :request do
+  path '/sessions' do
+    post 'Creates a session' do
+      tags 'Sessions'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+              email: { type: :string },
+              password: { type: :string }
+            },
+            required: %w[email password]
+        }
+
+      response '401', 'Invalid credentials' do
+        let(:user) { { user: attributes_for(:user, username: nil) } }
+        run_test!
+      end
+    end
+  end
+end
