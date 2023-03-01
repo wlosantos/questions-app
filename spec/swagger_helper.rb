0d1ec75536360cfe3f6395e4c30919ab4891cd5f
wendellopes <wendelopes@msn.com>
2023-03-01 09:38:07 -0300

    adjust host swagger

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -18,7 +18,7 @@ RSpec.configure do |config|
     'v1/swagger.yaml' => {
       openapi: '3.0.1',
       info: {
-        title: 'API V1',
+        title: 'Questions API',
         version: 'v1'
       },
       paths: {},
@@ -27,7 +27,7 @@ RSpec.configure do |config|
           url: 'https://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'www.example.com'
+              default: 'www.question-api.com'
             }
           }
         }
