438814d29b1b46ce7060b998eaccf6dbe152b6aa
wendellopes <wendelopes@msn.com>
2023-03-02 15:10:27 -0300

    create user_exams show document swagger

--- spec/integration/user_exams_spec.rb
+++ spec/integration/user_exams_spec.rb
@@ -5,7 +5,7 @@ describe 'UserExams API' do
   let(:token) { JwtAuth::TokenProvider.issue_token({ email: user.email }) }
   let(:Authorization) { "Bearer #{token}" }
   let(:exam) { create(:exam) }
-  let(:user_exam) { create(:user_exam, exam:) }
+  let(:user_exam) { create(:user_exam, exam:, user:) }
 
   # list all user_exams
   path '/user_exams' do
@@ -47,6 +47,64 @@ describe 'UserExams API' do
 
       response '401', 'unauthorized' do
         let(:Authorization) { nil }
+        run_test!
+      end
+    end
+  end
+
+  # show user_exam
+  path '/user_exams/{id}' do
+    get 'Show user_exam' do
+      tags 'User Exams'
+      security [Bearer: []]
+
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer, required: true
+
+      response '200', 'user_exam found' do
+        schema type: :object,
+               properties: {
+                 data: {
+                   type: :object,
+                   properties: {
+                     id: { type: :string },
+                     type: { type: :string },
+                     attributes: {
+                       type: :object,
+                       properties: {
+                         exam_id: { type: :integer },
+                         theme: { type: :string },
+                         score: { type: :float },
+                         user_answers: {
+                           type: :array,
+                           items: {
+                             type: :object,
+                             properties: {
+                               id: { type: :integer },
+                               question: { type: :integer },
+                               answer: { type: :integer },
+                               option: { type: :string },
+                               response: { type: :boolean }
+                             },
+                             required: %w[id question answer option response]
+                           }
+                         }
+                       },
+                       required: %w[exam-id theme score user-answers]
+                     }
+                   },
+                   required: %w[id type attributes]
+                 }
+               },
+               required: %w[data]
+
+        let(:id) { user_exam.id }
+        run_test!
+      end
+
+      response '401', 'unauthorized' do
+        let(:Authorization) { nil }
+        let(:id) { user_exam.id }
         run_test!
       end
     end
